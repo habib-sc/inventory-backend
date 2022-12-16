04b475a938094074a75227f15c2d31f89d492b0d
Habibur Rahman <habib97.cs@gmail.com>
2022-12-17 01:34:49 +0600

    added productSchema

--- app.js
+++ app.js
@@ -10,6 +10,80 @@ app.use(express.json());
 app.use(cors());
 
 
+// Schema design 
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name for this product"],
+        trim: true,  // extra space removing
+        unique: [true, "Name must be uniqe"],
+        minLength: [3, "Name must be at least 3 characters"],
+        maxLength: [100, "Name is too large"],
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, "Price can't be negetive"],
+    },
+    unit: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["Kg", "Litre", "Pcs"],
+            message: "unit value can't be {VALUE}, must be kg/litre/pcs"
+        },
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: [0, "Quantity can't be negative"],
+        validate: {
+            validator: (value) => {
+                const isInteger = Number.isInteger(value); // validatinog quantity
+                if (isInteger) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        message: "Quantity must be an integer"
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["in-stock", "out-stock", "discontinued"],
+            message: "Status can't be {VALUE}"
+        },
+    },
+    supplier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Supplier"
+    },
+    categories: [{
+        name: {
+            type: String,
+            required: true,
+        },
+        _id: mongoose.Schema.Types.ObjectId
+    }],
+    // createdAt: {
+    //     type: Date,
+    //     default: Date.now,
+    // },
+    // updatedAt: {
+    //     type: Date,
+    //     default: Date.now,
+    // }
+
+}, { timestamps: true });
+
+
 app.get("/", (req, res) => {
     res.send("Inventory Management Server");
 });
