8a75d65d433d14a73a4293d8caede8ad374dfd37
Habibur Rahman <habib97.cs@gmail.com>
2022-12-17 17:07:17 +0600

    update product by mongoose validation

--- services/product.services.js
+++ services/product.services.js
@@ -13,6 +13,10 @@ exports.createProductService = async (data) => {
 };
 
 exports.updateProductService = async (productId, data) => {
-    const result = await Product.updateOne({ _id: productId }, { $set: data });
+    // const result = await Product.updateOne({ _id: productId }, { $set: data  }, { runValidators: true });
+    // const result = await Product.updateOne({ _id: productId }, { $inc: data }, { runValidators: true });
+
+    const product = await Product.findById(productId);
+    const result = await product.set(data).save();
     return result;
 };
