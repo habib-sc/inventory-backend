e02086674ce50d4e663918996de4a11a88688081
Habibur Rahman <habib97.cs@gmail.com>
2022-12-30 21:23:46 +0600

    Added athorization

---
+++ middleware/authorization.js
@@ -0,0 +1,14 @@
+module.exports = (...role) => {
+    return (req, res, next) => {
+        const userRole = req.user.role;
+
+        if (!role.includes(userRole)) {
+            return res.status(403).json({
+                status: "Failed",
+                error: "You are not athorized"
+            })
+        }
+
+        next()
+    };
+};
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const productControler = require('../controllers/product.controller');
+const authorization = require('../middleware/authorization');
 
-const uploader = require('../middleware/uploader')
+const uploader = require('../middleware/uploader');
+const verifyToken = require('../middleware/verifyToken');
 
 router.route('/')
     .get(productControler.getProducts)
-    .post(productControler.createProduct)
+    .post(verifyToken, authorization("admin", "store-manager"), productControler.createProduct)
 
 router.route('/bulk-update')
     .patch(productControler.bulkUpdateProducts)
